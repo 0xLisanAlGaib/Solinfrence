3accc71a64ede96a4c9f40b4b4b4a1e4b20ffe06
0xLisanAlGaib <0xlisanalgaib@gmail.com>
2024-11-04 13:34:55 -0600

    fix: submodules corrected in remappings

--- src/Solinference.sol
+++ src/Solinference.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import { DSMath } from "lib/ds-math";
+import { DSMath } from "lib/ds-math/src/math.sol";
 import { ZTableMapping } from "./Z-mapping.sol";
 
 contract Solinference  is DSMath {
