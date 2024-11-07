fa30df2d24562114b73b2f3260c01673a8194b2f
0xLisanAlGaib <0xlisanalgaib@gmail.com>
2024-11-07 16:04:16 -0600

    Script improvement

--- script/DeploySolinference.s.sol
+++ script/DeploySolinference.s.sol
@@ -4,8 +4,15 @@ pragma solidity 0.8.20;
 
 import {Script} from "../lib/forge-std/src/Script.sol";
 import {Solinference} from "../src/Solinference.sol";
+import {ZTableMapping} from "../src/Z-mapping.sol";
 
 contract DeploySolinference is Script {
+    ZTableMapping private zTableInstance;
+
+    constructor(address _zTableMappingAddress) {
+        zTableInstance = ZTableMapping(_zTableMappingAddress);
+    }
+    address public Z_TABLE_MAPPING_ADDRESS = address(zTableInstance);
 
     function run() public returns (Solinference) {
         vm.startBroadcast();
